f7bc7fe235cfb65f68718915d7c8942d6d7b415a
didadadida93 <didadadida93@gmail.com>
2020-08-05 13:12:00 +0700

    implement prettier on *.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const PORT = process.env.PORT || 3000
-const app = express()
+const express = require("express");
+const PORT = process.env.PORT || 3000;
+const app = express();
 
-app.get('/', function(req, res) {
-  res.send({ message: 'Hello World!' })
-})
+app.get("/", function(req, res) {
+  res.send({ message: "Hello World!" });
+});
 
 app.listen(PORT, function() {
-  console.log("listening on port", PORT)
-})
+  console.log("listening on port", PORT);
+});
 
-module.exports = app
+module.exports = app;
--- test/test-app.js
+++ test/test-app.js
@@ -1,21 +1,21 @@
-const chai = require('chai')
-const chaiHttp = require('chai-http')
-const expect = chai.expect
+const chai = require("chai");
+const chaiHttp = require("chai-http");
+const expect = chai.expect;
 
-const app = require('../app')
+const app = require("../app");
 
-chai.use(chaiHttp)
+chai.use(chaiHttp);
 
-describe('testing app.js', function() {
-  describe('testing GET / - (code: 200)', function(){
-    it('should return object { message: \'Hello World!\' }', async function() {
-      const response = await chai.request(app).get('/')
+describe("testing app.js", function() {
+  describe("testing GET / - (code: 200)", function() {
+    it("should return object { message: 'Hello World!' }", async function() {
+      const response = await chai.request(app).get("/");
 
-      expect(response).to.have.status(200)
-      expect(response).to.have.property('body')
-      expect(response.body).to.be.an('object')
-      expect(response.body).to.have.property('message')
-      expect(response.body.message).to.equal('Hello World!')
-    })
-  })
-})
+      expect(response).to.have.status(200);
+      expect(response).to.have.property("body");
+      expect(response.body).to.be.an("object");
+      expect(response.body).to.have.property("message");
+      expect(response.body.message).to.equal("Hello World!");
+    });
+  });
+});
