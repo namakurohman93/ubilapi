c404b224d6b8e0760047902cbb121fb2a759d2ce
didadadida93 <didadadida93@gmail.com>
2020-08-05 12:15:04 +0700

    add app for testing

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const PORT = process.env.PORT || 3000
+const app = express()
+
+app.get('/', function(req, res) {
+  res.send({ message: 'Hello World!' })
+})
+
+app.listen(PORT, function() {
+  console.log("listening on port", PORT)
+})
+
+module.exports = app
