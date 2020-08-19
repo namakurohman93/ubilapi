f943a7e65d07375bad8cdfdaae29cf14f076731c
didadadida93 <didadadida93@gmail.com>
2020-08-19 11:51:37 +0700

    separating app and add development environment

--- app.js
+++ app.js
@@ -1,11 +1,6 @@
 const express = require("express");
-const PORT = process.env.PORT || 3000;
 const app = express();
 
 app.use("/", require("./routes"));
-
-app.listen(PORT, function() {
-  console.log("listening on port", PORT);
-});
 
 module.exports = app;
