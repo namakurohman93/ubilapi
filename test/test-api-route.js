f5b08d252f9ff66b138b38dc435e6ae23623aa7e
didadadida93 <didadadida93@gmail.com>
2020-08-26 14:56:27 +0700

    add testing for api route

--- test/test-api-route.js
+++ test/test-api-route.js
@@ -20,4 +20,19 @@ describe("testing /api route", function() {
       );
     });
   });
+
+  describe("testing GET /top-100 - (code: 200)", function() {
+    it("should return object response contain top 100 playlist", async function() {
+      const response = await chai.request(app).get("/api/top-100");
+
+      expect(response).to.have.status(200);
+      expect(response).to.have.property("body");
+      expect(response.body).to.be.an("object");
+      expect(response.body).to.have.property("response");
+      expect(response.body.response).to.be.an("object");
+      expect(response.body.response).to.have.property("top-100");
+      expect(response.body.response["top-100"]).to.be.an("array");
+      expect(response.body.response["top-100"].length).to.equal(100);
+    });
+  });
 });
