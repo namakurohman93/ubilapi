0656d9bcd55d170c28b1f24345d4a6dc653e5d8f
didadadida93 <didadadida93@gmail.com>
2020-08-28 18:38:51 +0700

    add feature based on last testing

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,5 +3,16 @@ const router = require("express").Router();
 router.get("/", function(req, res) {
   res.send({ message: "Hello World! you are on api routes." });
 });
+router.get("/top-100", function(req, res) {
+  // first thing first, this is just dummy route
+  // next update it will start implement billboard module
+
+  let top100 = [...new Array(100)].map(_ => 1)
+  res.send({
+    response: {
+      "top-100": top100
+    }
+  })
+})
 
 module.exports = router;
